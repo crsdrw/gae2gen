92412e4035bbf7fd9c45cb892e2c76194e9c966d
cdrew <cdrew@slb.com>
2018-10-16 21:19:05 +0100

    Init go module with hello world GAE app

---
+++ go.mod
@@ -0,0 +1,1 @@
+module github.com/crsdrw/gae2gen
---
+++ internal/handlers/handlers.go
@@ -0,0 +1,14 @@
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprint(w, "Hello, World!")
+}
